2d17b02c67cd2dcf794f31d0d4765f1743521adf
akash <asakashrohit8@gmail.com>
2023-08-03 13:22:27 +0530

    logout

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -6,7 +6,7 @@ export function SignOutButton() {
   const { instance } = useMsal();
 
   const handleSignIn = useCallback(() => {
-    instance.logoutRedirect();
+    instance.logout();
   }, [instance]);
 
   return (
