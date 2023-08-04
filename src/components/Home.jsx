a0476abd496faf1a3a456ec46e76cc25c9d58736
akash <asakashrohit8@gmail.com>
2023-08-04 16:39:33 +0530

    change password

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,11 +12,23 @@ import "./Home.css";
 
 export function Home() {
   const [loading, setLoading] = useState(false);
+  const [changePassword, setChangePassword] = useState(false);
   const { instance } = useMsal();
+
+  //https://myaccount.wegmans.com/wegmansonline.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1A_WegmansChangePassword&state=B2C_1A_WegmansChangePassword&client_id=7c0edc2c-5aa9-4a85-9ab0-ae11c5bb251e&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fshop.wegmans.com%2Fsocial-redirect%2Fwegmans_idp&scope=7c0edc2c-5aa9-4a85-9ab0-ae11c5bb251e%20openid%20offline_access&response_type=code
+
   const handleSso = useCallback(() => {
     setLoading(true);
     instance.loginRedirect({
       redirectUri: "https://introspecttestwebpoc.azurewebsites.net/",
+    });
+  }, [instance]);
+
+  const handleChangePassword = useCallback(() => {
+    setChangePassword(true);
+    instance.loginRedirect({
+      authority:
+        "https://stagingmyaccount.wegmans.com/stagewegmansonline.onmicrosoft.com/B2C_1A_WegmansChangePassword",
     });
   }, [instance]);
 
@@ -39,6 +51,15 @@ export function Home() {
           color="primary"
         >
           Test SSO
+        </LoadingButton>
+        <LoadingButton
+          onClick={handleChangePassword}
+          loading={changePassword}
+          disabled={changePassword}
+          variant="contained"
+          color="primary"
+        >
+          Change Password
         </LoadingButton>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
